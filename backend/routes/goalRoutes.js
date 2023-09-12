de9d37290cc4c39efd32a4355f56b8a96c759fe7
Noni86kg <nonisc2@gmail.com>
2023-09-12 07:21:37 +0200

    fished backend

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -2,12 +2,14 @@ const express = require("express");
 const router = express.Router();
 const {
   getGoals,
-  setGoals,
+  setGoal,
   updateGoal,
   deleteGoal,
 } = require("../controllers/goalController");
 
-router.route("/").get(getGoals).post(setGoals); // is the same as router.get("/", getGoals); router.post("/", setGoals);
-router.route("/:id").put(updateGoal).delete(deleteGoal);
+const { protect } = require("../middleware/authMiddleware");
+
+router.route("/").get(protect, getGoals).post(protect, setGoal);
+router.route("/:id").delete(protect, deleteGoal).put(protect, updateGoal);
 
 module.exports = router;
